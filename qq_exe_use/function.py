a9555eb12e6c2762c419bcf8e3180eb592c0a31b
三个和尚 <ahaufox@vip.qq.com>
2018-09-13 11:40:29 +0800

    测试

--- qq_exe_use/function.py
+++ qq_exe_use/function.py
@@ -4,7 +4,55 @@ import urllib
 import requests
 import time
 import re
-import config
+# -*- coding:gb2312 -*-
+import pymysql
+
+def mysql_local_select(sql):
+    try:
+        conn = pymysql.connect(host='127.0.0.1',
+                               port=3306,
+                               user='root',
+                               password='root',
+                               db='company_tel',
+                               charset='utf8',
+                               cursorclass=pymysql.cursors.DictCursor)
+        cur = conn.cursor()
+    except pymysql.Error,e:
+        return e
+    else:
+        try:
+            cur.execute(sql)
+            re=cur.fetchall()
+            cur.close()
+            conn.close()
+        except conn.Error, e:
+            return e
+        else:
+            return re
+
+def mysql_local_update_insert(sql):
+    try:
+        conn = pymysql.connect(host='127.0.0.1',
+                               port=3306,
+                               user='root',
+                               password='root',
+                               db='company_tel',
+                               charset='utf8',
+                               cursorclass=pymysql.cursors.DictCursor)
+        cur = conn.cursor()
+    except pymysql.Error,e:
+        return e
+    else:
+        try:
+            cur.execute(sql)
+            conn.commit()
+            cur.close()
+            conn.close()
+        except conn.Error, e:
+            return e
+        else:
+            return 1
+
 
 
 #判断特定id的cookies是否有效
